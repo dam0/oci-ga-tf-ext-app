0f5139ce4396b8ea243592a50dae5bc09b405c66
Clark Damian <damian.clark@ga.gov.au>
2025-07-23 15:03:40 +1000

    chore: Remove user_ocid and fingerprint variables from variables.tf

--- oci-terraform-ansible/variables.tf
+++ oci-terraform-ansible/variables.tf
@@ -6,15 +6,15 @@ variable "tenancy_ocid" {
   type        = string
 }
 
-variable "user_ocid" {
-  description = "The OCID of the user"
-  type        = string
-}
+# variable "user_ocid" {
+#   description = "The OCID of the user"
+#   type        = string
+# }
 
-variable "fingerprint" {
-  description = "The fingerprint of the public key"
-  type        = string
-}
+# variable "fingerprint" {
+#   description = "The fingerprint of the public key"
+#   type        = string
+# }
 
 variable "private_key_path" {
   description = "The path to the private key file"
